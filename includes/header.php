0610a5b59371713339af7e32acca96cf0b0e2148
Asif Hossain <asifiscoding@gmail.com>
2024-11-19 23:26:49 +0600

    index updated

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,162 @@
-<div class="navbar navbar-inverse set-radius-zero" >
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if($_SESSION['login']!=''){
+  $_SESSION['login']='';
+}
+if(isset($_POST['login']))
+{
+  $email=$_POST['emailid'];
+  $password=md5($_POST['password']);
+  $sql ="SELECT EmailId,Password,StudentId,Status FROM tblstudents WHERE EmailId=:email and Password=:password";
+  $query= $dbh -> prepare($sql);
+  $query-> bindParam(':email', $email, PDO::PARAM_STR);
+  $query-> bindParam(':password', $password, PDO::PARAM_STR);
+  $query-> execute();
+  $results=$query->fetchAll(PDO::FETCH_OBJ);
+
+  if($query->rowCount() > 0)
+  {
+    foreach ($results as $result) {
+      $_SESSION['stdid']=$result->StudentId;
+      if($result->Status==1)
+      {
+        $_SESSION['login']=$_POST['emailid'];
+        echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
+      } else {
+        echo "<script>alert('Your Account Has been blocked .Please contact admin');</script>";
+      }
+    }
+  } 
+  else {
+    echo "<script>alert('Invalid Details');</script>";
+  }
+}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>FEC Central Library </title>
+  <!-- BOOTSTRAP CORE STYLE  -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
+  <!-- FONT AWESOME STYLE  -->
+  <link href="assets/css/font-awesome.css" rel="stylesheet" />
+  <!-- CUSTOM STYLE  -->
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <!-- GOOGLE FONT -->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+    <style>
+
+    .library-header {
+    background-color: #002b5c; /* Blue color */
+    color: white;
+    padding: 20px 0;
+    }
+
+    .library-header .container {
+    max-width: 1200px;
+    margin: 0 auto;
+    }
+
+    .header-logo {
+    max-width: 100px;
+    margin-bottom: 10px;
+    }
+
+    .library-title {
+    font-size: 2em;
+    font-weight: bold;
+    margin-bottom: 5px;
+    }
+
+    .library-subtitle,
+    .library-address {
+    font-size: 1.2em;
+    }
+
+    .library-details {
+    margin-top: 10px;
+    font-size: 0.9em;
+    }
+
+    .library-fec-link {
+    color: #cce4ff;
+    }
+
+    .library-fec-link:hover {
+    text-decoration: underline;
+    }
+
+    .library-footer {
+        background-color: #2b3e50; /* Blue color */
+        color: white;
+        padding: 20px 0;
+    }
+
+    .library-footer .container {
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    .library-footer a {
+        color: #cce4ff; /* Light blue for links */
+        text-decoration: none;
+    }
+
+    .library-footer a:hover {
+        text-decoration: underline;
+    }
+
+    .library-footer p {
+        margin: 5px 0;
+    }
+
+    .library-footer .mt-3 {
+        margin-top: 15px;
+    }
+
+    .library-footer .footer-logo {
+        width: 120px; /* Adjust the size as needed */
+        margin-bottom: 10px;
+    }
+
+    </style>
+
+</head>
+<header class="library-header">
+  <div class="container">
+    <div class="row align-items-center">
+      <!-- Left Logo -->
+      <div class="col-md-2 col-sm-3 text-center">
+        <img src="./assets/img/feccl.png" alt="FEC Logo" class="header-logo">
+      </div>
+
+      <!-- Center Text -->
+      <div class="col-md-8 col-sm-6 text-center">
+        <h1 class="library-title">FEC Central Library</h1>
+        <p class="library-subtitle">Faridpur Engineering College Central Library</p>
+        <a href="http://www.fec.ac.bd"  class="library-fec-link" target="_blank">www.fec.ac.bd</a><br>
+      </div>
+
+      <!-- Right Logo -->
+      <div class="col-md-2 col-sm-3 text-center">
+        <img src="./assets/img/feccl.png" alt="FEC Logo" class="header-logo">
+      </div>
+    </div>
+  </div>
+</header>
+
+<body>
+  <!------MENU SECTION START-->
+  <?php if($_SESSION['login']) { ?>
+    <div class="navbar navbar-inverse set-radius-zero">
         <div class="container">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -6,68 +164,52 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" >
-
+                <a class="navbar-brand">
                     <img src="assets/img/logo.png" />
                 </a>
-
             </div>
-<?php if($_SESSION['login'])
-{
-?> 
             <div class="right-div">
                 <a href="logout.php" class="btn btn-danger pull-right">LOG ME OUT</a>
             </div>
-            <?php }?>
         </div>
     </div>
-    <!-- LOGO HEADER END-->
-<?php if($_SESSION['login'])
-{
-?>    
-<section class="menu-section">
+    <section class="menu-section">
         <div class="container">
-            <div class="row ">
+            <div class="row">
                 <div class="col-md-12">
-                    <div class="navbar-collapse collapse ">
+                    <div class="navbar-collapse collapse">
                         <ul id="menu-top" class="nav navbar-nav navbar-right">
                             <li><a href="dashboard.php" class="menu-top-active">DASHBOARD</a></li>
                             <li><a href="issued-books.php">Issued Books</a></li>
-                             <li>
+                            <li>
                                 <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Account <i class="fa fa-angle-down"></i></a>
                                 <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="my-profile.php">My Profile</a></li>
                                      <li role="presentation"><a role="menuitem" tabindex="-1" href="change-password.php">Change Password</a></li>
                                 </ul>
                             </li>
-
                         </ul>
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
-    <?php } else { ?>
-        <section class="menu-section">
+  <?php } else { ?>
+    <section class="menu-section">
         <div class="container">
-            <div class="row ">
+            <div class="row">
                 <div class="col-md-12">
-                    <div class="navbar-collapse collapse ">
+                    <div class="navbar-collapse collapse">
                         <ul id="menu-top" class="nav navbar-nav navbar-right">                        
-                          
-      <li><a href="index.php">Home</a></li>
-      <li><a href="index.php#ulogin">User Login</a></li>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="signin.php">User Login</a></li>
                             <li><a href="signup.php">User Signup</a></li>
-                         
                             <li><a href="adminlogin.php">Admin Login</a></li>
-
                         </ul>
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
-
-    <?php } ?>
+  <?php } ?>
+  <!-- MENU SECTION END-->
--- index.php
+++ index.php
@@ -3,148 +3,132 @@ session_start();
 error_reporting(0);
 include('includes/config.php');
 if($_SESSION['login']!=''){
-$_SESSION['login']='';
+  $_SESSION['login']='';
 }
 if(isset($_POST['login']))
 {
+  $email=$_POST['emailid'];
+  $password=md5($_POST['password']);
+  $sql ="SELECT EmailId,Password,StudentId,Status FROM tblstudents WHERE EmailId=:email and Password=:password";
+  $query= $dbh -> prepare($sql);
+  $query-> bindParam(':email', $email, PDO::PARAM_STR);
+  $query-> bindParam(':password', $password, PDO::PARAM_STR);
+  $query-> execute();
+  $results=$query->fetchAll(PDO::FETCH_OBJ);
 
-$email=$_POST['emailid'];
-$password=md5($_POST['password']);
-$sql ="SELECT EmailId,Password,StudentId,Status FROM tblstudents WHERE EmailId=:email and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':email', $email, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-
-if($query->rowCount() > 0)
-{
- foreach ($results as $result) {
- $_SESSION['stdid']=$result->StudentId;
-if($result->Status==1)
-{
-$_SESSION['login']=$_POST['emailid'];
-echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
-} else {
-echo "<script>alert('Your Account Has been blocked .Please contact admin');</script>";
-
+  if($query->rowCount() > 0)
+  {
+    foreach ($results as $result) {
+      $_SESSION['stdid']=$result->StudentId;
+      if($result->Status==1)
+      {
+        $_SESSION['login']=$_POST['emailid'];
+        echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
+      } else {
+        echo "<script>alert('Your Account Has been blocked .Please contact admin');</script>";
+      }
+    }
+  } 
+  else {
+    echo "<script>alert('Invalid Details');</script>";
+  }
 }
-}
-
-} 
-
-else{
-echo "<script>alert('Invalid Details');</script>";
-}
-}
-
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Online Library Management System | </title>
-    <!-- BOOTSTRAP CORE STYLE  -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <!-- FONT AWESOME STYLE  -->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <!-- CUSTOM STYLE  -->
-    <link href="assets/css/style.css" rel="stylesheet" />
-    <!-- GOOGLE FONT -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>FEC Central Library </title>
+  <!-- BOOTSTRAP CORE STYLE  -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
+  <!-- FONT AWESOME STYLE  -->
+  <link href="assets/css/font-awesome.css" rel="stylesheet" />
+  <!-- CUSTOM STYLE  -->
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <!-- GOOGLE FONT -->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
 
 </head>
+
 <body>
-    <!------MENU SECTION START-->
-<?php include('includes/header.php');?>
-<!-- MENU SECTION END-->
-<div class="content-wrapper">
-<div class="container">
-<!--Slider---->
-     <div class="row">
-              <div class="col-md-10 col-sm-8 col-xs-12 col-md-offset-1">
-                    <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel" >
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <img src="assets/img/1.jpg" alt="" />
-                        </div>
-                        <div class="item">
-                            <img src="assets/img/2.jpg" alt="" />
-                        </div>
-                        <div class="item">
-                            <img src="assets/img/3.jpg" alt="" /> 
-                        </div>
-                    </div>
-                    <!--INDICATORS-->
-                     <ol class="carousel-indicators">
-                        <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example" data-slide-to="1"></li>
-                        <li data-target="#carousel-example" data-slide-to="2"></li>
-                    </ol>
-                    <!--PREVIUS-NEXT BUTTONS-->
-                     <a class="left carousel-control" href="#carousel-example" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
-                </div>
+  <!------MENU SECTION START-->
+  <?php include('includes/header.php');?>
+  <!-- MENU SECTION END-->
+  <marquee behavior="scroll" direction="left" style="background-color: #EBEAFF; padding: 10px; font-size: 20px; font-weight: bold;">
+  Welcome to FEC Central Library - Your gateway to knowledge and innovation. Open 9 AM to 9 PM on all working days.
+  </marquee>
+
+
+  <div class="content-wrapper">
+    <div class="container">
+      <div class="row">
+        <!-- Left Column -->
+        <div class="col-md-8">
+          <!--Slider-->
+          <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="item active">
+                <img src="assets/img/1.jpg" alt="" />
               </div>
-             </div>
-<hr />
+              <div class="item">
+                <img src="assets/img/2.jpg" alt="" />
+              </div>
+              <div class="item">
+                <img src="assets/img/3.jpg" alt="" />
+              </div>
+            </div>
+            <!--INDICATORS-->
+            <ol class="carousel-indicators">
+              <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+              <li data-target="#carousel-example" data-slide-to="1"></li>
+              <li data-target="#carousel-example" data-slide-to="2"></li>
+            </ol>
+            <!--PREVIOUS-NEXT BUTTONS-->
+            <a class="left carousel-control" href="#carousel-example" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+          </div>
+        </div>
 
-
-
-<div class="row pad-botm">
-<div class="col-md-12">
-<h4 class="header-line">USER LOGIN FORM</h4>
-</div>
-</div>
- <a name="ulogin"></a>            
-<!--LOGIN PANEL START-->           
-<div class="row">
-<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
-<div class="panel panel-info">
-<div class="panel-heading">
- LOGIN FORM
-</div>
-<div class="panel-body">
-<form role="form" method="post">
-
-<div class="form-group">
-<label>Enter Email id</label>
-<input class="form-control" type="text" name="emailid" required autocomplete="off" />
-</div>
-<div class="form-group">
-<label>Password</label>
-<input class="form-control" type="password" name="password" required autocomplete="off"  />
-<p class="help-block"><a href="user-forgot-password.php">Forgot Password</a></p>
-</div>
-
-
-
- <button type="submit" name="login" class="btn btn-info">LOGIN </button> | <a href="signup.php">Not Register Yet</a>
-</form>
- </div>
-</div>
-</div>
-</div>  
-<!---LOGIN PABNEL END-->            
-             
- 
+        <!-- Right Column -->
+        <div class="col-md-4">
+          <div class="right-column-content" style="background-color: #2b3e50; color: white; padding: 15px; border-radius: 8px;">
+            <h4>Welcome to Faridpur Engineering College Library</h4>
+            <p>The library is transitioning to a fully automated system using Koha, with a collection of the latest engineering books, journals, and newspapers. It accommodates 72 users at a time and serves over 50 users daily.</p>
+            <p><strong>Services:</strong></p>
+            <ul>
+              <li>Library Membership</li>
+              <li>Lending Books</li>
+              <li>Renewal Books</li>
+              <li>Internet Service</li>
+              <li>Thesis Papers</li>
+            </ul>
+            <p><strong>Library Hours:</strong></p>
+            <p>Open 9:00 am - 5:00 pm (Lunch Break: 1:00 pm - 2:00 pm) on working days. Closed on Fridays & national holidays.</p>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-     <!-- CONTENT-WRAPPER SECTION END-->
- <?php include('includes/footer.php');?>
-      <!-- FOOTER SECTION END-->
-    <script src="assets/js/jquery-1.10.2.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
-    <script src="assets/js/bootstrap.js"></script>
-      <!-- CUSTOM SCRIPTS  -->
-    <script src="assets/js/custom.js"></script>
+  </div>
+
+
+  <!-- CONTENT-WRAPPER SECTION END-->
+  <?php include('includes/footer.php');?>
+  <!-- FOOTER SECTION END-->
+  <script src="assets/js/jquery-1.10.2.js"></script>
+  <!-- BOOTSTRAP SCRIPTS  -->
+  <script src="assets/js/bootstrap.js"></script>
+  <!-- CUSTOM SCRIPTS  -->
+  <script src="assets/js/custom.js"></script>
 
 </body>
+
 </html>
